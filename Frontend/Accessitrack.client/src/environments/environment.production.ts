f9d211042dcbad6f786f4426b76a8b53e76b3e6e
Philippe Charron <philippecharron1990@gmail.com>
2026-04-03 20:25:00 -0400

    feat: URL API production dans environment.production.ts

--- Frontend/Accessitrack.client/src/environments/environment.production.ts
+++ Frontend/Accessitrack.client/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://accessitrack-api.azurewebsites.net',
+  apiUrl: 'https://accessitrack-api-dea0b3dgddg7eng0.canadacentral-01.azurewebsites.net',
 };
